a7056742373fa5a8f8978a746c9e514088672cdd
Una <ruwen.hsieh@gmail.com>
2026-05-10 01:04:07 +0800

    增加輪播圖及左下角 Fixed Element

--- 03_eztravel_layout/script.js
+++ 03_eztravel_layout/script.js
@@ -20,4 +20,20 @@ document.addEventListener('DOMContentLoaded',() => {
             }
         }); 
     });
+
+
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+
+        pagination: {
+            el: ".swiper-pagination",
+        },
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
 });
